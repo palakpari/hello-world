4286fc93b82fe933c10a69c64e66ad013a15bf65
Nilesh Patel <palnil@gmail.com>
2019-11-11 10:59:09 -0500

    initial checkin

---
+++ src/main/java/com/yoshallc/hello/hello_world/App.java
@@ -0,0 +1,15 @@
+package com.yoshallc.hello.hello_world;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        
+    	
+    	
+    }
+}

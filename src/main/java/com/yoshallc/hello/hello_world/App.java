30dfd271719ad72855d8cfa57b7e939eb2dc1b8c
Nilesh Patel <palnil@gmail.com>
2019-11-11 18:48:06 -0500

    test1

--- src/main/java/com/yoshallc/hello/hello_world/App.java
+++ src/main/java/com/yoshallc/hello/hello_world/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world post commit test");
+        System.out.println("Hello world post commit test1");
     	
     	
     }

23e40cd1274e7f95fc6b1015b104d39c2082d5d6
Nilesh Patel <palnil@gmail.com>
2019-11-11 18:35:01 -0500

    dfdddgd

--- src/main/java/com/yoshallc/hello/hello_world/App.java
+++ src/main/java/com/yoshallc/hello/hello_world/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world post commit test234554");
+        System.out.println("Hello world post commit test2");
     	
     	
     }
